965880aae99a424a2b8d9204738813108bb9573f
Stranathan <nimsec@ian.local>
2020-10-21 14:36:30 -0400

    footer whitespace fix

--- rendering/shaders/backgroundRaymarch.js
+++ rendering/shaders/backgroundRaymarch.js
@@ -55,7 +55,16 @@ float raymarch(vec3 ro, vec3 rd)
     }
     return t;
 }
-
+float when_gt(float x, float y) 
+{
+    return max(sign(x - y), 0.0);
+}
+float random (vec2 st) 
+{
+    return fract(sin(dot(st.xy,
+                         vec2(12.9898,78.233)))
+                 * 43758.5453123);
+}
 void main()
 {
     vec2 uv = (gl_FragCoord.xy - 0.5 * resolution.xy)/ resolution.y;
@@ -67,7 +76,8 @@ void main()
 
     float dist = raymarch(ro, rd);
 
-    float fog = 1.0 / (0.3 * (1. + abs(sin(0.5 * time))) + dist * dist * 0.002);
+    float fog = 1.0 / (0.3 + (dist * dist * 0.002));
+    fog = clamp(fog, 0., 1.3);    
 
     fragColor = vec4(fog * vec3(0.43, 0.50, 0.56),1.0);
 }
